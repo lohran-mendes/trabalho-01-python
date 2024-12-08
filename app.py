453dfd0194fc75d9ab7ca4236cebd5ff3c2fd737
Lohran Mendes <lohranfm@gmail.com>
2024-12-07 22:48:38 -0300

    chore: question 1 solved

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
 import os
 
+def encerrar():
+  print('o valor inserido não é válido.\n')
+  print('Encerrando programa...\n')
+
 def exercicio_1():
   os.system('cls')
-  valor_base = int(input('Informe aqui o valor base: '))
-  idade = int(input('Qual a sua idade? '))
-  porcentagem = 1
-  print(f'Você tem {idade} anos')
+  print('Esse sistema foi criado por Lohran Fellipe Mendes de Souza')
+  
+  # tratamento de entrada dos dados
+  try: 
+    valor_base = float(input('Informe aqui o valor base do plano: R$ ').strip())
+  except:
+    encerrar()
+    return 
+  try:
+    idade = int(input('Digite a idade do cliente: ').strip())
+  except:
+    encerrar()  
+    return 
+  porcentagem = 0
 
+  # lógica da porcentagem de desconto oferecido ao cliente
   if type(idade) == int:
     if  0 <= idade < 19:
-      porcentagem = 1
-      print('primeira condição')
-    if  19 <= idade < 29:
-      porcentagem = 1.5
-      print('segunda condição')
+      porcentagem = 100 / 100
+    elif  19 <= idade < 29:
+      porcentagem = 150 / 100
     elif  29 <= idade < 39:
-      porcentagem = 2.25
-      print('terceira condição')
+      porcentagem = 225 / 100
     elif  39 <= idade < 49:
-      porcentagem = 2.40
-      print('quarta condição')
+      porcentagem = 240 / 100
     elif  49 <= idade < 59:
-      porcentagem = 3.50
-      print('quinta condição')
+      porcentagem = 350 / 100
     elif  59 <= idade:
-      porcentagem = 6.00
-      print('sexta condição')
+      porcentagem = 600 / 100
+    else:
+      encerrar()
+      return 
 
   valor_mensal_do_plano = valor_base * porcentagem
-  print(f'O valor mensal do plano será de {valor_mensal_do_plano} reais')
+  print(f'O valor mensal do plano será de R${valor_mensal_do_plano:.2f}\n')
 
 exercicio_1()
